386f4ece17523ba361aa86374d79d300f8231e2f
Hamsikakrishnan <r.hamsikakrishnan@gmail.com>
2025-07-31 14:09:24 +0530

    API Changes

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// fetch('http://www.omdbapi.com/?i=tt3896198&apikey=f10ea125&s=Conjuring')
+// fetch('http://www.omdbapi.com/?i=&apikey=&s=Conjuring')
 // .then(res => res.json())
 // .then(data => console.log(data))
